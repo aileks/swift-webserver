581ded26c6553ca3b4b03b805e76742f57b8a50b
aileks <88669313+aileks@users.noreply.github.com>
2024-02-19 19:23:31 -0500

    this is cool

--- Package.swift
+++ Package.swift
@@ -2,39 +2,34 @@
 import PackageDescription
 
 let package = Package(
-    name: "swift-vapor",
-    platforms: [
-       .macOS(.v13)
-    ],
-    dependencies: [
-        // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.89.0"),
-        // 🗄 An ORM for SQL and NoSQL databases.
-        .package(url: "https://github.com/vapor/fluent.git", from: "4.8.0"),
-        // 🪶 Fluent driver for SQLite.
-        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
-        // 🍃 An expressive, performant, and extensible templating language built for Swift.
-        .package(url: "https://github.com/vapor/leaf.git", from: "4.2.4"),
-    ],
-    targets: [
-        .executableTarget(
-            name: "App",
-            dependencies: [
-                .product(name: "Fluent", package: "fluent"),
-                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
-                .product(name: "Leaf", package: "leaf"),
-                .product(name: "Vapor", package: "vapor"),
-            ]
-        ),
-        .testTarget(name: "AppTests", dependencies: [
-            .target(name: "App"),
-            .product(name: "XCTVapor", package: "vapor"),
+  name: "swift-vapor",
+  platforms: [
+    .macOS(.v13)
+  ],
+  dependencies: [
+    .package(url: "https://github.com/vapor/vapor.git", from: "4.89.0")
+  ],
+  targets: [
+    .executableTarget(
+      name: "App",
+      dependencies: [
+        .product(name: "Fluent", package: "fluent"),
+        .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+        .product(name: "Leaf", package: "leaf"),
+        .product(name: "Vapor", package: "vapor"),
+      ]
+    ),
+    .testTarget(
+      name: "AppTests",
+      dependencies: [
+        .target(name: "App"),
+        .product(name: "XCTVapor", package: "vapor"),
 
-            // Workaround for https://github.com/apple/swift-package-manager/issues/6940
-            .product(name: "Vapor", package: "vapor"),
-            .product(name: "Fluent", package: "Fluent"),
-            .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
-            .product(name: "Leaf", package: "leaf"),
-        ])
-    ]
+        // Workaround for https://github.com/apple/swift-package-manager/issues/6940
+        .product(name: "Vapor", package: "vapor"),
+        .product(name: "Fluent", package: "Fluent"),
+        .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+        .product(name: "Leaf", package: "leaf"),
+      ]),
+  ]
 )
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,13 +2,35 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async throws in
-        try await req.view.render("index", ["title": "Hello Vapor!"])
-    }
+  app.get { req async throws in
+    try await req.view.render("index", ["title": "Hello Vapor!"])
+  }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
-    }
+  app.get("hello", ":name") { req async throws -> String in
+    let name = try req.parameters.require("name")
+    return "Hello, \(name.capitalized)!"
+  }
 
-    try app.register(collection: TodoController())
+  app.get("json", ":name") { req async throws -> UserResponse in
+    let name = try req.parameters.require("name")
+    let message = "Hello, \(name.capitalized)!"
+    return UserResponse(message: message)
+  }
+
+  app.post("user-info") { req async throws -> UserResponse in
+    let userInfo = try req.content.decode(UserInfo.self)
+    let message = "Hello, \(userInfo.name) you are \(userInfo.age) years old!"
+    return UserResponse(message: message)
+  }
+
+  try app.register(collection: TodoController())
+}
+
+struct UserResponse: Content {
+  let message: String
+}
+
+struct UserInfo: Content {
+  let name: String
+  let age: Int
 }
